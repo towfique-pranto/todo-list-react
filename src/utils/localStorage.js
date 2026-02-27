1e15556fcf4cee8addcf2e9456752a86016ff955
Towfique Kabir <towfique.pranto@gmail.com>
2026-02-28 00:40:37 +0600

    chore: cleanup code

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,6 +1,6 @@
 export function setItem(key, value) {
   try {
-    window.localStorage.setItem(key, JSON.stringify(value));
+    localStorage.setItem(key, JSON.stringify(value));
 
   } catch (error) {
     console.log(error);
@@ -9,7 +9,7 @@ export function setItem(key, value) {
 
 export function getItem(key) {
   try {
-    const item = window.localStorage.getItem(key);
+    const item = localStorage.getItem(key);
     return item ? JSON.parse(item) : undefined;
   } catch (error) {
     console.log(error);
